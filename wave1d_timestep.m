239da0efed8540c3ce1cf05bf2a5ed867b65a8c8
Antoine Pomari <A.Pomari@student.tudelft.nl>
2021-05-15 15:41:23 +0200

    Initial Code

---
+++ wave1d_timestep.m
@@ -0,0 +1,9 @@
+function newx=wave1d_timestep(x,i,settings)
+%function newx=timestep(x,i,settings)
+%return (h,u) one timestep later
+%take one timestep
+    A=settings.A;
+    B=settings.B;
+    rhs=B*x;
+    rhs(1)=settings.h_left(i); %left boundary, take at t+dt
+    newx=A\rhs;
